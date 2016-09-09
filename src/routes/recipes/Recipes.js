6d810a9793d08762d93b6d65023c91860ab39372
Andy Haynes <andyghaynes@gmail.com>
2016-09-08 20:10:15 -0700

    recipe page tabs

--- src/routes/recipes/Recipes.js
+++ src/routes/recipes/Recipes.js
@@ -2,17 +2,24 @@ import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Recipes.css';
 import SavedRecipesContainer from '../../containers/SavedRecipesContainer';
+import Tabs from 'material-ui/Tabs/Tabs';
+import Tab from 'material-ui/Tabs/Tab';
 
 function Recipes(props, context) {
   context.setTitle('Saved Recipes');
   return (
     <div className={s.recipes}>
-      Saved Recipes
-      <div className="pure-g">
-        <div className="pure-u-1-2">
+      <Tabs>
+        <Tab label="Saved">
           <SavedRecipesContainer />
-        </div>
-      </div>
+        </Tab>
+        <Tab label="Shared">
+        </Tab>
+        <Tab label="Public">
+        </Tab>
+        <Tab label="Parse">
+        </Tab>
+      </Tabs>
     </div>
   );
 }
