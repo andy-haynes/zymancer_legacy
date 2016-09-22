5978bc266276cb26748d44193450cd6dde88cb10
Andy Haynes <andyghaynes@gmail.com>
2016-09-21 23:49:32 -0700

    cleaner  header

--- src/components/RecipeHeader/RecipeHeader.js
+++ src/components/RecipeHeader/RecipeHeader.js
@@ -6,12 +6,14 @@ import Measurement from '../Measurement';
 import { roundTo } from '../../utils/core';
 import SliderInput from '../SliderInput';
 import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem'
 import ContentSave from 'material-ui/svg-icons/content/save';
 import { grey200 } from 'material-ui/styles/colors';
 
 const RecipeHeader = ({ recipe, setRecipeName, setTargetVolume, setBoilVolume, setBoilTime, setEfficiency, saveRecipe }) => (
   <div className={s.recipeHeader}>
-    <div className={s.headers}>
+    <div>
       <div className="pure-g">
         <div className="pure-u-1-24">
           <ContentSave
@@ -20,64 +22,101 @@ const RecipeHeader = ({ recipe, setRecipeName, setTargetVolume, setBoilVolume, s
             onClick={saveRecipe}
           />
         </div>
-        <div className="pure-u-5-24">Recipe Name</div>
-        <div className="pure-u-2-24">OG</div>
-        <div className="pure-u-2-24">FG</div>
-        <div className="pure-u-1-24">IBU</div>
-        <div className="pure-u-1-24">ABV</div>
-        <div className="pure-u-3-24">Target Volume</div>
-        <div className="pure-u-3-24">Boil Volume</div>
-        <div className="pure-u-2-24">Boil Time</div>
-        <div className="pure-u-4-24">Efficiency</div>
+        <div className="pure-u-6-24">
+          <TextField id="recipe-name" placeholder="Recipe Name" value={recipe.name} onChange={e => setRecipeName(e.target.value)} />
+        </div>
+        <div className="pure-u-1-24">
+          <div className={s.headerLabel}>
+            OG
+          </div>
+        </div>
+        <div className="pure-u-2-24">
+          <div className={s.calculatedValue}>
+            {recipe.originalGravity === 1 ? '1.000' : `${recipe.originalGravity.toString()}000`.substring(0, 5)}
+          </div>
+        </div>
+        <div className="pure-u-1-24">
+          <div className={s.headerLabel}>
+            IBU
+          </div>
+        </div>
+        <div className="pure-u-2-24">
+          <div className={s.calculatedValue}>
+            {roundTo(recipe.IBU, 1)}
+          </div>
+        </div>
+        <div className="pure-u-1-24">
+          <div className={s.headerLabel}>
+            Target
+          </div>
+        </div>
+        <div className="pure-u-3-24">
+          <Measurement measurement={recipe.targetVolume} update={setTargetVolume} options={RecipeVolume} />
+        </div>
+        <div className="pure-u-2-24">
+          <div className={s.headerLabel}>
+            Efficiency
+          </div>
+        </div>
+        <div className="pure-u-5-24">
+          <SliderInput
+            value={recipe.efficiency}
+            min={25} max={95}
+            update={setEfficiency}
+            sliderWidth={'3-4'}
+            inputWidth={'1-4'}
+          />
+        </div>
       </div>
     </div>
     <div className="pure-g">
       <div className="pure-u-1-24"></div>
-      <div className="pure-u-5-24">
-        <TextField id="recipe-name" value={recipe.name} onChange={e => setRecipeName(e.target.value)} />
+      <div className="pure-u-6-24">
+        <SelectField value="old">
+          <MenuItem key="1" value="old" primaryText="Old Ale" />
+          <MenuItem key="2" value="new" primaryText="New Ale" />
+        </SelectField>
+      </div>
+      <div className="pure-u-1-24">
+        <div className={s.headerLabel}>
+          FG
+        </div>
       </div>
       <div className="pure-u-2-24">
         <div className={s.calculatedValue}>
-          {recipe.originalGravity}
+          {recipe.finalGravity === 1 ? '1.000' : `${recipe.finalGravity.toString()}000`.substring(0, 5)}
+        </div>
+      </div>
+      <div className="pure-u-1-24">
+        <div className={s.headerLabel}>
+          ABV
         </div>
       </div>
       <div className="pure-u-2-24">
-        <div className={s.calculatedValue}>
-          {recipe.finalGravity}
-        </div>
-      </div>
-      <div className="pure-u-1-24">
-        <div className={s.calculatedValue}>
-          {roundTo(recipe.IBU, 1)}
-        </div>
-      </div>
-      <div className="pure-u-1-24">
         <div className={s.calculatedValue}>
           {recipe.ABV}%
         </div>
       </div>
-      <div className="pure-u-3-24">
-        <Measurement measurement={recipe.targetVolume} update={setTargetVolume} options={RecipeVolume} />
+      <div className="pure-u-1-24">
+        <div className={s.headerLabel}>
+          Boil
+        </div>
       </div>
       <div className="pure-u-3-24">
         <Measurement measurement={recipe.boilVolume} update={setBoilVolume} options={RecipeVolume} />
       </div>
       <div className="pure-u-2-24">
+        <div className={s.headerLabel}>
+          Boil Time
+        </div>
+      </div>
+      <div className="pure-u-5-24">
         <TextField
           id="boil-minutes"
           className={s.recipeInput}
           value={recipe.boilMinutes}
           onChange={e => setBoilTime(e.target.value)}
           style={{width: "40px"}}
-        />
-      </div>
-      <div className="pure-u-4-24">
-        <SliderInput
-          value={recipe.efficiency}
-          min={25} max={95}
-          update={setEfficiency}
-          sliderWidth={'3-4'}
-          inputWidth={'1-4'}
         />
       </div>
     </div>
