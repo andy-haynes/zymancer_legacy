74071f65b1bdf55b56fb3272156cd2500251f265
Andy Haynes <andyghaynes@gmail.com>
2016-09-24 12:15:23 -0700

    drop/create debugging

--- src/data/models/index.js
+++ src/data/models/index.js
@@ -62,58 +62,58 @@ Yeast.belongsToMany(Recipe, { through: RecipeYeast, as: 'recipes', foreignKey: '
 
 function sync(...args) {
   if (process.env.NODE_ENV !== 'production') {
-    //return sequelize.sync(...args)
-    let currentIngredientId = 0;
-    return sequelize.sync(Object.assign({}, args, { force: false }));
-      /*
-            .then(() => {
-              return User.findOrCreate({
-                where: {
-                  id: 'c42ccdc0-806d-11e6-a049-657457c8144e',
-                  email: 'andyghaynes@gmail.com',
-                  emailConfirmed: true
-                }
-              });
-            }).then(() => {
-              return Grain.bulkCreate(Ingredients.filter(i => i.ingredientType === 1).map(grain => ({
-                name: grain.name,
-                category: grain.category,
-                gravity: grain.gravity,
-                lovibond: grain.lovibond,
-                description: grain.description
-              })));
-            }).then(() => {
-              return Hop.bulkCreate(Ingredients.filter(i => i.ingredientType === 2).map(hop => ({
-                name: hop.name,
-                aroma: hop.aroma.join(','),
-                categories: hop.categories.join(','),
-                url: hop.url,
-                alpha: hop.alpha,
-                beta: hop.beta,
-                coHumulone: hop.coHumulone,
-                totalOil: hop.totalOil,
-                myrcene: hop.myrcene,
-                caryophyllene: hop.caryophyllene,
-                farnesene: hop.farnesene,
-                humulene: hop.humulene,
-                geraniol: hop.geraniol
-              })));
-            }).then(() => {
-              return Yeast.bulkCreate(Ingredients.filter(i => i.ingredientType === 3).map(yeast => ({
-                name: yeast.name,
-                code: yeast.code,
-                url: yeast.url,
-                description: yeast.description,
-                flocculation: yeast.flocculation,
-                rangeF: yeast.rangeF,
-                rangeC: yeast.rangeC,
-                tolerance: yeast.tolerance,
-                attenuation: yeast.attenuation,
-                mfg: yeast.mfg,
-                styles: yeast.styles.join(',')
-              })));
-            });
-            */
+    const clean = false;
+
+    if (!clean) {
+      return sequelize.sync(...args);
+    } else {
+      return sequelize.sync(Object.assign({}, args, { force: true }))
+        .then(() =>  User.create({
+          id: 'c17bd4a0-8288-11e6-9448-dd60cdc5f340',
+          email: 'andyghaynes@gmail.com',
+          emailConfirmed: true
+        })).then(() => UserLogin.create({
+          name: 'google',
+          key: '112708624366974007964',
+          userId: 'c17bd4a0-8288-11e6-9448-dd60cdc5f340'
+        })).then(() => UserClaim.create({
+          type: 'urn:google:access_token',
+          value: 'ya29.CjBoA4VfDDBaobEg88kaAsLu_i8GxedUA7rRLn5zZoU6XB2CDcwAWrrMji7mHm9WmXM',
+          userId: 'c17bd4a0-8288-11e6-9448-dd60cdc5f340'
+        })).then(() => Grain.bulkCreate(Ingredients.filter(i => i.ingredientType === 1).map(grain => ({
+          name: grain.name,
+          category: grain.category,
+          gravity: grain.gravity,
+          lovibond: grain.lovibond,
+          description: grain.description
+        })))).then(() => Hop.bulkCreate(Ingredients.filter(i => i.ingredientType === 2).map(hop => ({
+          name: hop.name,
+          aroma: hop.aroma.join(','),
+          categories: hop.categories.join(','),
+          url: hop.url,
+          alpha: hop.alpha,
+          beta: hop.beta,
+          coHumulone: hop.coHumulone,
+          totalOil: hop.totalOil,
+          myrcene: hop.myrcene,
+          caryophyllene: hop.caryophyllene,
+          farnesene: hop.farnesene,
+          humulene: hop.humulene,
+          geraniol: hop.geraniol
+        })))).then(() => Yeast.bulkCreate(Ingredients.filter(i => i.ingredientType === 3).map(yeast => ({
+          name: yeast.name,
+          code: yeast.code,
+          url: yeast.url,
+          description: yeast.description,
+          flocculation: yeast.flocculation,
+          rangeF: yeast.rangeF,
+          rangeC: yeast.rangeC,
+          tolerance: yeast.tolerance,
+          attenuation: yeast.attenuation,
+          mfg: yeast.mfg,
+          styles: yeast.styles.join(',')
+        }))));
+    }
   } else {
     return sequelize.sync(...args);
   }
