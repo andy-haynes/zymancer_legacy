aba3d2b1c98b7176c67ab40f681965f0c909c913
Andy Haynes <andyghaynes@gmail.com>
2017-01-27 00:36:49 -0800

    dedupe yeast sources

--- src/constants/TestIngredients.js
+++ src/constants/TestIngredients.js
@@ -9,6 +9,7 @@ const yeast = Object.keys(wyeast).map(k => wyeast[k])
   .concat(Object.keys(whiteLabs).map(k => whiteLabs[k]))
   .concat(Object.keys(imperial).map(k => imperial[k]))
   .reduce((a, b) => a.concat(b), [])
+  .reduce((arr, yeast) => (!arr.some(y => y.url === yeast.url && y.code === yeast.code) && arr.push(yeast) && arr) || arr, [])
   .map(y => Object.assign(y, {
     toleranceLow: (y.tolerance || y.toleranceLow) || null,
     ingredientType: 3
