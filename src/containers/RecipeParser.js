c82228981399340e5e7889278e3694f459c98f6d
Andy Haynes <andyghaynes@gmail.com>
2017-05-06 16:28:54 -0700

    override parsed fields that won't come in from ingredient matches (e.g. weight)

--- src/containers/RecipeParser.js
+++ src/containers/RecipeParser.js
@@ -1,10 +1,10 @@
 import { connect } from 'react-redux';
 import RecipeParser from '../components/RecipeParser';
 import actions from '../actions';
-
 import grain from '../reducers/grain';
 import hop from '../reducers/hop';
 import yeast from '../reducers/yeast';
+import pick from 'lodash/pick';
 
 function mapState(state) {
   return {
@@ -15,13 +15,23 @@ function mapState(state) {
 
 function mapDispatch(dispatch) {
   function loadParsedRecipe(recipe) {
-    const takeFirstSuggestion = (ingredient) => ingredient.suggestions[0];
+    function buildSuggestion(ingredient, overrideProps = []) {
+      const selectedGrain = ingredient.suggestions[0];  //TODO take the one matched by the user
+      const parsedFields = pick(ingredient, overrideProps);
 
-    recipe.grains = recipe.grains.map(takeFirstSuggestion).map(grain.create);
-    recipe.hops = recipe.hops.map(takeFirstSuggestion).map(hop.create);
-    recipe.fermentation = {
-      yeasts: recipe.yeast.map(takeFirstSuggestion).map(yeast.create)
-    };
+      Object.keys(parsedFields).forEach(overrideKey => {
+        if (parsedFields[overrideKey]) {
+          selectedGrain[overrideKey] = parsedFields[overrideKey];
+        }
+      });
+
+      return selectedGrain;
+    }
+
+    recipe.grains = recipe.grains.map(g => buildSuggestion(g, ['name', 'weight', 'lovibond'])).map(grain.create);
+    recipe.hops = recipe.hops.map(h => buildSuggestion(h, ['name', 'alpha', 'beta', 'additions'])).map(hop.create);
+    recipe.fermentation = { yeasts: recipe.yeast.map(buildSuggestion).map(yeast.create) };
+
     dispatch(actions.saved.loadSavedRecipe(recipe));
   }
 
